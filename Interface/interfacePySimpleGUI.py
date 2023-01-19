290ba6c7b9d81782b78426c23f2b2f33f2da1ecd
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2023-01-19 14:20:15 -0300

    Aula de python

--- Interface/interfacePySimpleGUI.py
+++ Interface/interfacePySimpleGUI.py
@@ -17,15 +17,16 @@ Janela = sg.Window("Sistema de cotação",layout)
 #Repetição enquanto for verdadeiro sistema ficará repetindo a ação da tela
 while True:
     #Crar as variavel para pegar os eventos e valores de ação na tela
-    evento, valores = Janela.read()
+    evento,  valores = Janela.read()
     #Evento para encerrar a janela
     if evento == sg.WIN_CLOSED or evento == "Cancelar":
         break
     #Evento de limpar campo informa o nome da tela e entre conchetes colocar a chave que foi criada e atualizar 
     if evento == "Limpar":
         Janela["nome_cotacao"].update("")
+        Janela["texto_cotacao"].update("")
     if evento == "Pegar cotação":
-        codigo_cotacao = valores["nome_cotacao"]
+        codigo_cotacao = valores["nome_cotacao"].upper()
         #Chamar outra class com a busca de cotação e retornar o valor da cotação da moeda desejada
         cotacao = pegar_cotacao(codigo_cotacao)
         Janela["texto_cotacao"].update(f"A cotação do {codigo_cotacao} é de R$ {cotacao}")
---
+++ Scripti Python/teste01.py
@@ -0,0 +1,8 @@
+nome = input("Qual é seu nome? ")
+print ("Seja bem vindo " , nome)
+dia = input("Informe o dia do seu nascimento? ")
+mes = input("Informe o mês do seu nascimento? ")
+ano = input("Informe o ano do seu nascimento? ")
+print("Você nasceu no dia " , dia ," no mês " , mes ," do ano " , ano)
+
+
