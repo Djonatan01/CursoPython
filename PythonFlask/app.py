dbdadf36ed977fa5e2fd794fc196a2ed26907ea4
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2023-04-06 09:29:39 -0300

    Update app.py

--- PythonFlask/app.py
+++ PythonFlask/app.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template ,request
+from flask_sqlalchemy import SQLAlchemy
+
+
 #Definição de nme da aplicação
 app = Flask (__name__)
+# create the extension
+db = SQLAlchemy(app)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cursos.db"
 
 recebeFrutas = []
 registros = []
 Item = []
 
+class cursos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    descricao = db.Column(db.String(100))
+    ch = db.Column(db.Integer)
+    
+    def __init__(self,nome,descricao,ch):
+        self.nome = nome
+        self.descricao = descricao
+        self.ch = ch
+    
 @app.route('/', methods=["GET" , "POST"])
 def principal():
     if request.method == "POST":
@@ -30,6 +49,7 @@ def sobre():
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
     
 #set FLASK_ENV=development serve para ativar o modo debug    
