750e9eef63551d346a54ac5f60e51842280677fb
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2023-07-13 11:19:12 -0300

    Update app.py

--- PythonFlask/app.py
+++ PythonFlask/app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template ,request
 from flask_sqlalchemy import SQLAlchemy
 
-
 #Definição de nme da aplicação
 app = Flask (__name__)
-# create the extension
-db = SQLAlchemy(app)
 
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cursos.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cursos.sqlite3"
+#Crição do banco de dados SQLAlchemy
+db = SQLAlchemy(app)
 
 recebeFrutas = []
 registros = []
@@ -19,17 +18,17 @@ class cursos(db.Model):
     nome = db.Column(db.String(50))
     descricao = db.Column(db.String(100))
     ch = db.Column(db.Integer)
-    
+
     def __init__(self,nome,descricao,ch):
         self.nome = nome
         self.descricao = descricao
         self.ch = ch
-    
+
 @app.route('/', methods=["GET" , "POST"])
 def principal():
     if request.method == "POST":
         if request.form.get("fruta"):
-            recebeFrutas.append(request.form.get("fruta")) #método append serve para inserir dados ao fim de uma lista; 
+            recebeFrutas.append(request.form.get("fruta")) #método append serve para inserir dados ao fim de uma lista;
     return render_template("index.html" , recebeFrutas = recebeFrutas)
 
 @app.route('/itens',methods = ["GET" ,"POST"])
@@ -37,8 +36,8 @@ def itens():
     if request.method=="POST":
         if request.form.get("Item"):
             Item.append(request.form.get("Item"))
-    return render_template("itens.html", Item = Item)        
-    
+    return render_template("itens.html", Item = Item)
+
 @app.route('/sobre', methods = ["GET","POST"])
 def sobre():
     if request.method == "POST":
@@ -47,8 +46,7 @@ def sobre():
     return render_template("sobre.html",registros=registros)
 
 
-
 if __name__ == "__main__":
+    app.app_context().push()
     db.create_all()
     app.run(debug=True)
-    
